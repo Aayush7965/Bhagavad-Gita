6dbe6cc00eb7543ed057c56675a67b70dc876133
Aayush Pareek <aayushpareek@10.10.10.6>
2023-10-02 21:27:17 +0530

    Added Arrows for changing chapters

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
@@ -6,13 +7,15 @@ const date = new Date();
 const app = express();
 // When loading website from render.com it will use process.env.PORT to get PORT information from the website
 // If loading it on localhost it will use PORT 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 let versesData = [];
 // You have to add your own RapidAPI key, I am using "process.env.API_KEY" because I stored my API_KEY in render.com and this is the way to access it
 const API_KEY = process.env.API_KEY;
+let currentChapterNumber;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
 
 async function getData(chapterNumber, res) {
     versesData = [];
@@ -37,23 +40,44 @@ async function getData(chapterNumber, res) {
             // Push all the data into versesData array
             versesData.push(desiredBlock.description)
         }
+        currentChapterNumber = chapterNumber; // 1
+
+        let leftArrowData = currentChapterNumber;
+        let rightArrowData = currentChapterNumber;
+
+        if (rightArrowData == 18) {
+            rightArrowData = 1
+        } else {
+            rightArrowData ++;
+        }
+
+        if (leftArrowData == 1) {
+            leftArrowData = 18;
+        } else {
+            leftArrowData --;
+        }
+
 
         res.render("index.ejs", {
                   chapterNumber: data.chapter_number,
                   chapterName: data.name_translated,
                   chapterSummary: data.chapter_summary,
                   slokArray: versesData,
-                  currentYear: date.getFullYear()
+                  currentYear: date.getFullYear(),
+                  leftArrowData: leftArrowData,
+                  rightArrowData: rightArrowData
           });
+          
     } catch (error) {
         console.error("This is an error", error);
     }
 }
 
 app.get("/", async (req, res) => {
-    // Calling getData function for chapte 1
-    getData(1, res)
+    // Calling getData function for chapter 1
+    res.redirect(`/chapter/1`)
 })
+
 
 app.get("/chapter", (req, res) => {
     // If user click reload when in "/chapter" url it will redirect them to "/"
@@ -63,12 +87,28 @@ app.get("/chapter", (req, res) => {
 app.post("/chapter", (req, res) => {
     // Getting chapter number from our button pressed in hamburger menu e.x. Chapter 1, Chapter 2
     let originalString = req.body.chapter;
-    // Removing "Chapter " from our result e.x. Chapter 1 => 1, Chapter 5 => 5
-    const newString = originalString.replace("Chapter ", "");
-    // Sending these chapter numbers to our getData function to get data from our API for a specific chapter
-    getData(newString, res);
+    if (originalString) {
+        // Removing "Chapter " from our result e.x. Chapter 1 => 1, Chapter 5 => 5
+        const newString = originalString.replace("Chapter ", "");
+        // Sending these chapter numbers to our getData function to get data from our API for a specific chapter
+        // await getData(newString, res);
+        res.redirect(`/chapter/${newString}`)
+    } else {
+        res.redirect(`/chapter/1`)
+    }
 });
 
+app.get("/chapterNumber", (req, res) => {
+    const data = {
+        chapterNumber: currentChapterNumber
+    };
+    // Send the data as JSON to the client
+    res.json(data);
+})
+
+app.get("/chapter/:chapterNumber", (req, res) => {
+    getData(req.params.chapterNumber, res)
+})
 
 
 app.listen(port, () => {
